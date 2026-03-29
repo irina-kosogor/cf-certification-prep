1ec1a87d095ad0a83842a93b5f7a52a595d3611d
Iryna Kosogor <i.v.kosogor@gmail.com>
2026-03-29 13:41:52 +0300

    add all migration scripts

---
+++ migrations_full/06-edit-product-contentType-add-images-field.js
@@ -0,0 +1,24 @@
+/*
+
+add a new images field and disable editing for existing image field.
+
+*/
+
+const _ = require("lodash");
+
+module.exports = function(migration) {
+    const product = migration.editContentType("product");
+
+    const images = product
+        .createField("images")
+        .name("Images")
+        .type("Array")
+        .items({
+            type: "Link",
+            linkType: "Entry",
+            validations: [{ linkContentType: ["mediaWrapper"] }],
+        });
+
+    product.editField("image").disabled(true);
+};
+
